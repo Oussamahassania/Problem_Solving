28b7eb21734cf8d1cf86ebf90f4ff6867ae15666
ayoubxid <ayoub.idhaddouch.70@gmail.com>
2025-07-10 12:24:04 +0200

    this is  DIGITS problem

--- Digits.java
+++ Digits.java
@@ -1,4 +1,24 @@
-public class Digits {
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Scanner;
 
-    
+import static javax.management.Query.value;
+
+public class Digits {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter number");
+        int size=sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            int value=sc.nextInt();
+          while (value!=0){
+              int mynum=value%10;
+              System.out.print(mynum+" ");
+              value=value/10;
+          }
+            System.out.println();
+
+        }
+    }
 }
+
